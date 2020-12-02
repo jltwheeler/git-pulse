2c69730fab87ab2f95a22f9b2de38e557d2fa979
jltwheeler <jltwheeler@gmail.com>
2020-12-02 21:55:46 +0000

    Changed script name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 import yargs from "yargs/yargs";
 
 yargs(process.argv.slice(2))
-  .scriptName("Git Pulse")
+  .scriptName("git-pulse")
   .usage("Usage: $0 -r string -i string")
   .command("init", "initialises a user config file", () => {
     console.log("initial commit");
