a740729ffcc93163be061691e4b1957ab9b91aa9
jltwheeler <jltwheeler@gmail.com>
2020-12-29 15:45:40 +0000

    fixed --version automatic version reading from package.json

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@
 import yargs from "yargs/yargs";
 import chalk from "chalk";
 
+import { version } from "../package.json";
 import initCmd from "./commands/init";
 
 yargs(process.argv.slice(2))
-  .version(process.env?.npm_package_version || "0.0.0")
+  .version(version || "0.0.0")
   .scriptName("git-pulse")
   .usage("Usage: $0 -r string -i string")
   .command(initCmd)
