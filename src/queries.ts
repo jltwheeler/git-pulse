095ac2978824cf54d30b3cd3e1997ef73bbb8b71
jltwheeler <jltwheeler@gmail.com>
2020-12-30 22:51:32 +0000

    Removed redundant function and console logs

--- src/queries.ts
+++ src/queries.ts
@@ -1,15 +1,5 @@
 import { gql } from "graphql-request";
 
-export const createRepoQuery = (
-  prefix: number,
-  name: string,
-  owner: string,
-): string => {
-  return `request${prefix}: repository(name: "${name}", owner: "${owner}") {
-    description
-  }`;
-};
-
 export const VALIDATE_REPO = gql`
   query validateRepo($name: String!, $owner: String!) {
     repository(name: $name, owner: $owner) {
